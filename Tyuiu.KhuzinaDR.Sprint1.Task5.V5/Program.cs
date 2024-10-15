d45aadf4ae7dbbd8bb9045517ba87373461dc414
angeldrip9919 <i61angel>
2024-10-16 00:15:03 +0500

    Task5

---
+++ Tyuiu.KhuzinaDR.Sprint1.Task5.V5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KhuzinaDR.Sprint1.Task5.V5.Lib;
+
+namespace Tyuiu.KhuzinaDR.Sprint1.Task5.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт №1 | Выполнила:Хузина Д. Р. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Хузина Диана Ранидовна | СМАРТб-24-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу:                    *");
+            Console.WriteLine("* Присвоить целой переменной d первую цифру из дробной части              *");
+            Console.WriteLine("* положительного вещественного числа x (так, если x=32.597, то d=5).      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение Х");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = Convert.ToInt32(ds.Calculate(x));
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
