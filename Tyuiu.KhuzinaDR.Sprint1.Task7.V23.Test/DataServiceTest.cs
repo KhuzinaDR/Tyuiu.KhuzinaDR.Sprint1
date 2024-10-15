02afc8dc5b487b2e0260cdc8564212e7323254dd
angeldrip9919 <i61angel>
2024-10-16 01:45:18 +0500

    Task7

---
+++ Tyuiu.KhuzinaDR.Sprint1.Task7.V23.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.KhuzinaDR.Sprint1.Task7.V23.Lib;
+namespace Tyuiu.KhuzinaDR.Sprint1.Task7.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 10.0;
+            double y = 5.0;
+            double wait = 11.111;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res, 0.001);
+        }
+    }
+}
