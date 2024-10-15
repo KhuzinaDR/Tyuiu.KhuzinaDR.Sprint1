c199b67e9d87208a39f3f6df2f31da219ea7dc37
angeldrip9919 <i61angel>
2024-10-15 22:40:49 +0500

    Add project files.

---
+++ Tyuiu.KhuzinaDR.Sprint1.Task1.V21/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.KhuzinaDR.Sprint1.Task1.V21.Lib;
+namespace Tyuiu.KhuzinaDR.Sprint1.Task1.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт 1 | Выполнила: Хузина Д. Р. | СМАРТб-24-1";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт №1                                                                  *");
+            Console.WriteLine("* Тема :  Базовые навыки работы в C#                                         *");
+            Console.WriteLine("* Задание №1                                                                 *");
+            Console.WriteLine("* Вариант №21                                                                *");
+            Console.WriteLine("* Выполнила : Хузина Диана Ранидовна | СМАРТб-24-1                    *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ :                                                                  *");
+            Console.WriteLine("* Написать программу , которая запрашивает у пользователя исходные данные    *");
+            Console.WriteLine("* вычисляет результат по формуле (x * y) / (3 + y) и печатает его на экране  *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                          *");
+            Console.WriteLine("******************************************************************************");
+
+            double x, y;
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ :                                                                *");
+            Console.WriteLine("******************************************************************************");
+
+
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadLine();
+        }
+    }
+}
