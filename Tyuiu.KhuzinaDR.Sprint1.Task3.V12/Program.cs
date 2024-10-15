78da78ebb7282276de1080b44e15c579c2406d8c
angeldrip9919 <i61angel>
2024-10-15 23:00:33 +0500

    Task3

---
+++ Tyuiu.KhuzinaDR.Sprint1.Task3.V12/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KhuzinaDR.Sprint1.Task3.V12.Lib;
+
+namespace Tyuiu.KhuzinaR.Sprint1.Task3.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Хузина Д. Р. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выпонила: Хузина Диана Ранидовна | СМАРТб-24-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу для вычисления площади прямоугольного треугольника по*");
+            Console.WriteLine("* длинам двух катетов. Ответ округлите до 3 знаков после запятой.         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double lengthCathetus1 = 14.537;
+            double lengthCathetus2 = 19.382;
+
+
+            Console.WriteLine("Сторона катета 1 = " + lengthCathetus1);
+            Console.WriteLine("Сторона катета 2 = " + lengthCathetus2);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Площадь треугольника = " + ds.TriangleArea(lengthCathetus1, lengthCathetus2));
+
+            Console.ReadLine();
+        }
+    }
+}
