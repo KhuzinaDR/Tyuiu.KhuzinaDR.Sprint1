b46ca0d6c97e1729af94d98b32bffa346b30c349
angeldrip9919 <i61angel>
2024-10-15 22:48:42 +0500

    Task2

---
+++ Tyuiu.KhuzinaDR.Sprint1.Task2.V16/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.KhuzinaDR.Sprint1.Task2.V16.Lib;
+namespace Tyuiu.KhuzinaDR.Sprint1.Task2.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Хузина Д.Р | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки  работы в С#                                       *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Хузина Диана Ранидовна | СМАРТб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int r;
+            Console.WriteLine("\nВведите радиус круга:");
+            r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.CalculatePerimetrCircle(r));
+            Console.ReadKey();
+        }
+    }
+}
